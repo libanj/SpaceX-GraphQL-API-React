f151bfd3617e8040c6197008b8fa9fac99d57831
Liban <liban1808@gmail.com>
2021-09-21 22:09:33 +0100

    setup project and made components grid view

---
+++ src/ColumnItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ColumnItem() {
+	return (
+		<div className="item">
+			<img src="https://picsum.photos/200" alt="Mission" />
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, porro veniam? Perferendis, quisquam tenetur veritatis molestias eligendi iusto delectus incidunt!</p>
+		</div>
+	)
+}
+
+export default ColumnItem
